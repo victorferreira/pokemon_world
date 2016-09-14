c0b7a74895f1e8e3951341d14a4751278692c62c
Victor Ferreira <victor.araujo.ferreira@gmail.com>
2016-09-14 15:07:04 -0300

    add pokemon types and size; all tests passing

--- lib/pokemon.h
+++ lib/pokemon.h
@@ -2,14 +2,37 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef enum {NORMAL,
+              FIRE,
+              WATER,
+              GRASS,
+              ELECTRIC,
+              ICE,
+              FIGHTING,
+              POISON,
+              GROUND,
+              FLYING,
+              PSYCHIC,
+              BUG,
+              ROCK,
+              GHOST,
+              DARK,
+              DRAGON,
+              STEEL,
+              FAIRY
+} pokemon_type;
+
 typedef struct {
   int number;
   char* name;
-  int size;
+  double size;
+  pokemon_type pType; // TODO: implement dual type pokemon
 } tPokemon;
 
 
 int printPokemon(tPokemon* p) {
   printf("Number: %d\n", p->number);
   printf("Name: %s\n", p->name);
+  printf("Size: %.2fm\n", p->size);
+  printf("Type: %d\n", p->pType);
 }
--- test/test_pokemon.c
+++ test/test_pokemon.c
@@ -3,15 +3,21 @@
 int test_create_pokemon() {
   char* name = "pikachu";
   int number = 25;
+  double size = 0.4;
+  int type = PSYCHIC;
 
   tPokemon Pk;
   Pk.name = name;
   Pk.number = number;
+  Pk.pType = type;
+  Pk.size = size;
 
   if (Pk.name == name
-    && Pk.number == number) {
+    && Pk.number == number
+    && Pk.pType == type
+    && Pk.size == size) {
     printf("test_create_pokemon   passes\n");
-    // printPokemon(&Pk);
+    printPokemon(&Pk);
     return 0;
   } else {
     printf("test_create_pokemon   fails\n");
