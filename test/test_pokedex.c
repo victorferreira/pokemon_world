5e074807cdc564665d4543e450defa5e29311fac
Victor Ferreira <victor.araujo.ferreira@gmail.com>
2016-09-20 20:32:32 -0300

    cleanup

--- test/test_pokedex.c
+++ test/test_pokedex.c
@@ -2,24 +2,18 @@
 #include "../lib/pokedex.h"
 
 int test_insert_one_pokemon() {
-  int number = 25;
-  char* name = "Pikachu";
-  double size = 0.4;
-  double weight = 6.0;
-  char* type = "PSYCHIC";
 
   struct tPokemon Pk;
-  Pk = createPokemon(number, name, size, weight, type);
+  Pk = createPokemon(25, "Pikachu", 0.4, 6.0, "PSYCHIC");
 
   addPokemonToPokedex(&Pk);
 
-  if (Px.items[number].number == number
-    // && Px.items[number].name == name
-    && Px.items[number].size == size
-    && Px.items[number].weight == weight
-    && Px.items[number].pType == type) {
+  if (Px.items[Pk.number].number == Pk.number
+    && Px.items[Pk.number].name == Pk.name
+    && Px.items[Pk.number].size == Pk.size
+    && Px.items[Pk.number].weight == Pk.weight
+    && Px.items[Pk.number].pType == Pk.pType) {
     printf("test_insert_one_pokemon   passes\n");
-    // printPokemon(Px.pokemon[number]);
     return 0;
   } else {
     printf("test_insert_one_pokemon   fails\n");
@@ -38,17 +32,16 @@ int test_insert_two_pokemons() {
   addPokemonToPokedex(&Pk2);
 
   if (Px.items[Pk.number].number == Pk.number
-    // && Px.items[Pk.number].name == Pk.name
+    && Px.items[Pk.number].name == Pk.name
     && Px.items[Pk.number].size == Pk.size
     && Px.items[Pk.number].weight == Pk.weight
     && Px.items[Pk.number].pType == Pk.pType
     && Px.items[Pk2.number].number == Pk2.number
-    // && Px.items[Pk2.number].name == Pk2.name
+    && Px.items[Pk2.number].name == Pk2.name
     && Px.items[Pk2.number].size == Pk2.size
     && Px.items[Pk2.number].weight == Pk2.weight
     && Px.items[Pk2.number].pType == Pk2.pType) {
     printf("test_insert_two_pokemons   passes\n");
-    // printPokedex();
     return 0;
   } else {
     printf("test_insert_two_pokemons   fails\n");
